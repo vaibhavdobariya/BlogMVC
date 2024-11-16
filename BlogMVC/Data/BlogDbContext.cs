189155efc2ee607ff5d8e934059c672384d17420
vaibhavdobariya <vaibhavdobariya98@gmail.com>
2024-11-16 21:42:44 +0530

    Add project files.

---
+++ BlogMVC/Data/BlogDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BlogMVC.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogMVC.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
---
+++ BlogMVC/Models/Domain/BlogPost.cs
@@ -0,0 +1,19 @@
+﻿using Azure;
+
+namespace BlogMVC.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+        public string Heading { get; set; }
+        public string PageTitle { get; set; }
+        public string PageContent { get; set; }
+        public int ShortDescription { get; set; }
+        public string FeaturedImageUrl { get; set; }
+        public string UrlHandle { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Author { get; set; }
+        public bool Visible { get; set; }
+        public ICollection<Tag> Tags { get; set; }
+    }
+}
